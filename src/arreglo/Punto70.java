4bd54713604ddf26f149a322758e022f91817982
rhassmus <felipeagr_@hotmail.com>
2019-04-08 10:48:04 -0500

    prueba

---
+++ src/arreglo/Punto70.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arreglo;
+
+import java.util.Random;
+
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Punto70 {
+    public void operacion() {
+        Random r = new Random();
+        int [][] matriz = new int [10][10];
+        int temp=0;
+        //llenado de matriz
+        
+        
+    }
+}
